ec691f1059a5225b90ee061a4ddc5281d908acff
Edison T T <edison.thomas795@gmail.com>
2026-05-28 14:18:37 +0530

    adding build

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  exportVault: (data) => ipcRenderer.invoke('export-vault', data),
-  importVault: () => ipcRenderer.invoke('import-vault')
-});
